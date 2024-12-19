c9a157c91384df588561745faca3f02383a90ae1
ryanmas13 <156191935+ryanmas13@users.noreply.github.com>
2024-12-19 23:18:18 +0100

    Updated requirements.tx for new dependencies. Updated streamlit_app.py for new features.

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ Keep your answers concise and based on facts – do not hallucinate.""".strip()
 model = "gpt-4o-mini"
 
 # Set up Settings for LLM and Embeddings
-from langchain.embeddings.huggingface import HuggingFaceBgeEmbeddings
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from llama_index.core import Settings
 Settings.llm = OpenAI(model=model, temperature=0, system_prompt=prompt)
 Settings.embed_model = HuggingFaceBgeEmbeddings(model_name="BAAI/bge-base-en")
@@ -34,7 +34,7 @@ Settings.embed_model = HuggingFaceBgeEmbeddings(model_name="BAAI/bge-base-en")
 # Setup chat message history
 if "messages" not in st.session_state.keys(): # Initialize the chat message history
     st.session_state.messages = [
-        {"role": "assistant", "content": "Who are the parties involved in the contract?"},
+#         {"role": "assistant", "content": "Who are the parties involved in the contract?"},
     ]
 
 
@@ -44,13 +44,6 @@ def loadData():
     with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
-        # service_context = ServiceContext.from_defaults(
-        #     llm=OpenAI(
-        #             model=model,
-        #             temperature=0, 
-        #             system_prompt=prompt
-        #             )
-        #     )
         index = VectorStoreIndex.from_documents(docs)
         return index
 
@@ -60,7 +53,7 @@ index = loadData()
 
 # Set up the chat engine
 if "chatEngine" not in st.session_state.keys(): # Initialize the chat engine
-    st.session_state.chatEngine = index.create_chat_engine(chat_mode="openai", llm=model, verbose=True)
+    st.session_state.chatEngine = index.as_chat_engine(chat_mode="openai", verbose=True)
 
 
 
@@ -68,105 +61,17 @@ if "chatEngine" not in st.session_state.keys(): # Initialize the chat engine
 if prompt := st.chat_input("Ask a question about the contract"): # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-for message in st.session_state.messages: # Display the prior chat messages
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
+if "messages" in st.session_state.keys():
+    for message in st.session_state.messages: # Display the prior chat messages
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
 
 
-# If last message is not from assistant, generate a new response
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
-            response = st.session_state.chatEngine.stream_chat(prompt)
-            st.write_stream(response.response_gen)
-            message = {"role": "assistant", "content": response.response}
-            st.session_state.messages.append(message) # Add response to message history
-
-# # Split the provided document into chunks
-# from langchain.text_splitter import CharacterTextSplitter
-
-# def split_text(file):
-#     text_splitter = CharacterTextSplitter(
-#         separator="\n",
-#         chunk_size= 500,
-#         chunk_overlap = 20, 
-#         length_function = len
-#     )
-#     chunks = text_splitter.split_documents(file)
-#     return chunks
-
-# # Setup the LLM
-# # Add code here to set up the IBM LLM or OpenAI LL
-
-# # Define the prompt
-# from langchain import hub
-# prompt = hub.pull("rlm/rag-prompt")
-
-
-
-# # Let the user upload a file via `st.file_uploader`.
-# uploaded_pdf = st.file_uploader(
-#     "Upload a document (.pdf)", type=("pdf")
-# )
-
-# # For testing purposes, we will use a dummy pdf
-
-    
-# # Ask the user for a question via `st.text_area`.
-# question = st.text_area(
-#     "Now ask a question about the document!",
-#     placeholder="Can you give me a short summary of this contract?",
-#     disabled=not uploaded_pdf,
-# )
-
-# from PyPDF2 import PdfReader
-# from langchain_ibm import WatsonxEmbeddings
-# from langchain_community.vectorstores import FAISS
-# from langchain_core.output_parsers import StrOutputParser
-# from langchain_core.runnables import RunnablePassthrough
-
-
-# if uploaded_pdf and question:
-#     st.write("Here")
-#     pdf_reader = PdfReader(uploaded_pdf)
-#     text = ""
-#     for page in pdf_reader.pages:
-#       text += page.extract_text()
-#     st.write("Splitting text")
-#     #Split the incoming text
-#     text_splitter = CharacterTextSplitter(
-#         separator="\n",
-#         chunk_size= 500,
-#         chunk_overlap = 20, 
-#         length_function = len
-#     )
-#     chunks = text_splitter.split_text(text)
-#     #Create embeddings
-#     st.write("Creating embeddings")
-#     embeddings = WatsonxEmbeddings(
-#     model_id="ibm/slate-30m-english-rtrvr",
-#     url=credentials["url"],
-#     apikey=credentials["apikey"],
-#     project_id=projectID
-#     )
-#     #Setup vectorstore using FAISS
-#     ids = [str(i) for i in range(0, len(chunks))]
-#     faissdb = FAISS.from_texts(chunks, embeddings, ids=ids)
-#     #Setup retreiver
-#     retriever = faissdb.as_retriever()
-#     st.write("Setup the chain")
-#     llm = LLM()
-#     rag_chain = (
-#             {
-#                 "context": retriever,
-#                 "question": RunnablePassthrough()
-#             }
-#             | prompt
-#             | llm
-#             | StrOutputParser()
-#         )
-#     response = rag_chain.invoke(question)
-#     st.text(response)
-
-
-    
+    # If last message is not from assistant, generate a new response
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                response = st.session_state.chatEngine.stream_chat(prompt)
+                st.write_stream(response.response_gen)
+                message = {"role": "assistant", "content": response.response}
+                st.session_state.messages.append(message) # Add response to message history

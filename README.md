f2b27401bc9331b036a5822e2c98e6afd6c883d5
ryanmas13 <156191935+ryanmas13@users.noreply.github.com>
2024-12-19 13:19:19 +0100

    Updated code to use Llamaindex. Create folder for documents and updated requirements for llama index dependencies.

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # 📄 Document question answering template
 
-A simple Streamlit app that answers questions about an uploaded document via OpenAI's GPT-3.5.
-
-[![Open in Streamlit](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://document-question-answering-template.streamlit.app/)
+A simple Streamlit app that answers questions about an uploaded document via OpenAI's GPT-4o-mini.
 
 ### How to run it on your own machine
 
@@ -17,3 +15,16 @@ A simple Streamlit app that answers questions about an uploaded document via Ope
    ```
    $ streamlit run streamlit_app.py
    ```
+
+Additional information about Llamaindex chat engines: 
+LlamaIndex has four different chat engines:
+
+1. Condense question engine: Always queries the knowledge base. Can have trouble with meta questions like “What did I previously ask you?”
+
+2. Context chat engin": Always queries the knowledge base and uses retrieved text from the knowledge base as context for following queries. The retrieved context from previous queries can take up much of the available context for the current query.
+
+3. ReAct agent: Chooses whether to query the knowledge base or not. Its performance is more dependent on the quality of the LLM. You may need to coerce the chat engine to correctly choose whether to query the knowledge base.
+
+4. OpenAI agent: Chooses whether to query the knowledge base or not—similar to ReAct agent mode, but uses OpenAI’s built-in fuOpenAI'salling capabilities.
+
+We will use the OpenAI agent here. 
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,101 +1,174 @@
 import streamlit as st
-#from openai import OpenAI
+
+import streamlit as st
+from llama_index import VectorStoreIndex, ServiceContext, Document
+from llama_index.llms import OpenAI
+import openai
+from llama_index import SimpleDirectoryReader
 
 # Show title and description.
-st.title("📄 Document question answering")
-st.write(
-    "Upload your contract below and ask a question about it "
-)
+st.title("📄 Document question answering ")
+# st.write(
+#     "Upload your contract below and ask a question about it "
+# )
 
-# Get credentials
+# Get credentials, set up prompt and model
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+
+prompt = """
+You are an expert at analyzing contracts and your job is to answer technical questions about the contract. 
+Assume all questions are related to the provided contracts. 
+Keep your answers concise and based on facts – do not hallucinate.""".strip()
+
+model = "gpt-4o-mini"
+
+# Set up Settings for LLM and Embeddings
+from langchain.embeddings.huggingface import HuggingFaceBgeEmbeddings
+from llama_index.core import Settings
+Settings.llm = OpenAI(model=model, temperature=0, system_prompt=prompt)
+Settings.embed_model = HuggingFaceBgeEmbeddings(model_name="BAAI/bge-base-en")
+
+# Use OpenAI Embedding instead of HuggingFace by uncommenting the following lines
+# Settings.embed_model = OpenAIEmbedding(
+#     model="text-embedding-3-small", embed_batch_size=100
+# )
+
+# Setup chat message history
+if "messages" not in st.session_state.keys(): # Initialize the chat message history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Who are the parties involved in the contract?"},
+    ]
+
+
+# Load the data and create the index
+@st.cache_resource(show_spinner=False)
+def loadData():
+    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
+        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+        docs = reader.load_data()
+        # service_context = ServiceContext.from_defaults(
+        #     llm=OpenAI(
+        #             model=model,
+        #             temperature=0, 
+        #             system_prompt=prompt
+        #             )
+        #     )
+        index = VectorStoreIndex.from_documents(docs)
+        return index
+
+# Call load data function
+index = loadData()
+
+
+# Set up the chat engine
+if "chatEngine" not in st.session_state.keys(): # Initialize the chat engine
+    st.session_state.chatEngine = index.create_chat_engine(chat_mode="openai", llm=model, verbose=True)
 
 
 
-# Split the provided document into chunks
-from langchain.text_splitter import CharacterTextSplitter
+# Prompt for user input and save to chat history
+if prompt := st.chat_input("Ask a question about the contract"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
-def split_text(file):
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size= 500,
-        chunk_overlap = 20, 
-        length_function = len
-    )
-    chunks = text_splitter.split_documents(file)
-    return chunks
-
-# Setup the LLM
-# Add code here to set up the IBM LLM or OpenAI LL
-
-# Define the prompt
-from langchain import hub
-prompt = hub.pull("rlm/rag-prompt")
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
 
 
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = st.session_state.chatEngine.stream_chat(prompt)
+            st.write_stream(response.response_gen)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message) # Add response to message history
 
-# Let the user upload a file via `st.file_uploader`.
-uploaded_pdf = st.file_uploader(
-    "Upload a document (.pdf)", type=("pdf")
-)
+# # Split the provided document into chunks
+# from langchain.text_splitter import CharacterTextSplitter
 
-# For testing purposes, we will use a dummy pdf
+# def split_text(file):
+#     text_splitter = CharacterTextSplitter(
+#         separator="\n",
+#         chunk_size= 500,
+#         chunk_overlap = 20, 
+#         length_function = len
+#     )
+#     chunks = text_splitter.split_documents(file)
+#     return chunks
+
+# # Setup the LLM
+# # Add code here to set up the IBM LLM or OpenAI LL
+
+# # Define the prompt
+# from langchain import hub
+# prompt = hub.pull("rlm/rag-prompt")
+
+
+
+# # Let the user upload a file via `st.file_uploader`.
+# uploaded_pdf = st.file_uploader(
+#     "Upload a document (.pdf)", type=("pdf")
+# )
+
+# # For testing purposes, we will use a dummy pdf
 
     
-# Ask the user for a question via `st.text_area`.
-question = st.text_area(
-    "Now ask a question about the document!",
-    placeholder="Can you give me a short summary of this contract?",
-    disabled=not uploaded_pdf,
-)
+# # Ask the user for a question via `st.text_area`.
+# question = st.text_area(
+#     "Now ask a question about the document!",
+#     placeholder="Can you give me a short summary of this contract?",
+#     disabled=not uploaded_pdf,
+# )
 
-from PyPDF2 import PdfReader
-from langchain_ibm import WatsonxEmbeddings
-from langchain_community.vectorstores import FAISS
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
+# from PyPDF2 import PdfReader
+# from langchain_ibm import WatsonxEmbeddings
+# from langchain_community.vectorstores import FAISS
+# from langchain_core.output_parsers import StrOutputParser
+# from langchain_core.runnables import RunnablePassthrough
 
 
-if uploaded_pdf and question:
-    st.write("Here")
-    pdf_reader = PdfReader(uploaded_pdf)
-    text = ""
-    for page in pdf_reader.pages:
-      text += page.extract_text()
-    st.write("Splitting text")
-    #Split the incoming text
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size= 500,
-        chunk_overlap = 20, 
-        length_function = len
-    )
-    chunks = text_splitter.split_text(text)
-    #Create embeddings
-    st.write("Creating embeddings")
-    embeddings = WatsonxEmbeddings(
-    model_id="ibm/slate-30m-english-rtrvr",
-    url=credentials["url"],
-    apikey=credentials["apikey"],
-    project_id=projectID
-    )
-    #Setup vectorstore using FAISS
-    ids = [str(i) for i in range(0, len(chunks))]
-    faissdb = FAISS.from_texts(chunks, embeddings, ids=ids)
-    #Setup retreiver
-    retriever = faissdb.as_retriever()
-    st.write("Setup the chain")
-    llm = LLM()
-    rag_chain = (
-            {
-                "context": retriever,
-                "question": RunnablePassthrough()
-            }
-            | prompt
-            | llm
-            | StrOutputParser()
-        )
-    response = rag_chain.invoke(question)
-    st.text(response)
+# if uploaded_pdf and question:
+#     st.write("Here")
+#     pdf_reader = PdfReader(uploaded_pdf)
+#     text = ""
+#     for page in pdf_reader.pages:
+#       text += page.extract_text()
+#     st.write("Splitting text")
+#     #Split the incoming text
+#     text_splitter = CharacterTextSplitter(
+#         separator="\n",
+#         chunk_size= 500,
+#         chunk_overlap = 20, 
+#         length_function = len
+#     )
+#     chunks = text_splitter.split_text(text)
+#     #Create embeddings
+#     st.write("Creating embeddings")
+#     embeddings = WatsonxEmbeddings(
+#     model_id="ibm/slate-30m-english-rtrvr",
+#     url=credentials["url"],
+#     apikey=credentials["apikey"],
+#     project_id=projectID
+#     )
+#     #Setup vectorstore using FAISS
+#     ids = [str(i) for i in range(0, len(chunks))]
+#     faissdb = FAISS.from_texts(chunks, embeddings, ids=ids)
+#     #Setup retreiver
+#     retriever = faissdb.as_retriever()
+#     st.write("Setup the chain")
+#     llm = LLM()
+#     rag_chain = (
+#             {
+#                 "context": retriever,
+#                 "question": RunnablePassthrough()
+#             }
+#             | prompt
+#             | llm
+#             | StrOutputParser()
+#         )
+#     response = rag_chain.invoke(question)
+#     st.text(response)
 
 
     
